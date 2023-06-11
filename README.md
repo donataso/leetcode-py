f55d381ea3543e6a589ca6233e2876d97f0482de
Donatas <donatas.o@gmail.com>
2023-06-11 15:53:13 +0200

    initial setup and readme

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # leetcode-py
+
+Here I'm solving leetcode problems in python.
+
+My collection of solutions in php is in another repo: https://github.com/donataso/leetcode
---
+++ problems/_skeleton.py
@@ -0,0 +1,33 @@
+import unittest
+from unittest_data_provider import data_provider  # type: ignore
+
+
+class Solution:
+    """
+    url
+    difficulty: easy|medium|hard
+    """
+
+    def solve(self, *args):
+        pass
+
+
+class SolutionTestCase(unittest.TestCase):
+    class SolutionTestCase(unittest.TestCase):
+        @staticmethod
+        def dataprovider():
+            yield (
+                # params
+                [],
+                # expected
+                [],
+            )
+
+        @data_provider(dataprovider)
+        def test_solution(self, params, expected):
+            solution = Solution()
+            self.assertEqual(expected, solution.solve(*params))
+
+
+if __name__ == '__main__':
+    unittest.main()
