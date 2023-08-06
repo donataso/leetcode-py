9e0eee7bc0252a0ee9b1e30ed24df5037eedf44e
Donatas <donatas.o@gmail.com>
2023-08-06 17:35:26 +0200

    some matrices

---
+++ problems/n0498_diagonal_traverse.py
@@ -0,0 +1,71 @@
+import unittest
+from unittest_data_provider import data_provider  # type: ignore
+
+
+class Solution:
+    """
+    https://leetcode.com/problems/diagonal-traverse/
+    difficulty: medium
+    """
+
+    def findDiagonalOrder(self, mat: list[list[int]]) -> list[int]:
+        return self.easier_solution_adapted_from_1224_problem(mat)
+        # return self.my_original_solution(mat)
+
+    def easier_solution_adapted_from_1224_problem(self, mat: list[list[int]]) -> list[int]:
+        result: list[list[int]] = []
+
+        for m in range(len(mat)):
+            for n in range(len(mat[m])):
+                if m + n == len(result):
+                    result.append([])
+                result[m + n].append(mat[m][n])
+
+        return [num for i, row in enumerate(result) for num in (reversed(row) if i % 2 == 0 else row)]
+
+    def my_original_solution(self, mat: list[list[int]]) -> list[int]:
+        n_rows = len(mat)
+        n_cols = len(mat[0])
+        result: list[int] = []
+
+        for d in range(n_rows + n_cols - 1):
+            diag: list[int] = []
+            m = min(d, n_rows - 1)  # once we reach the last row, we stay at it
+            start_col = max(0, d - n_rows + 1)  # once we reach the last row, our starting column shifts right
+            for n in range(start_col, n_cols):
+                if m < 0:
+                    break
+                diag.append(mat[m][n])
+                m -= 1
+
+            if d % 2 == 1:
+                diag.reverse()
+            result.extend(diag)
+
+        return result
+
+
+class SolutionTestCase(unittest.TestCase):
+    @staticmethod
+    def dataprovider():
+        yield (
+            # params
+            [[[1, 2, 3], [4, 5, 6], [7, 8, 9]]],
+            # expected
+            [1, 2, 4, 7, 5, 3, 6, 8, 9],
+        )
+        yield (
+            # params
+            [[[1, 2], [3, 4]]],
+            # expected
+            [1, 2, 3, 4],
+        )
+
+    @data_provider(dataprovider)
+    def test_solution(self, params, expected):
+        solution = Solution()
+        self.assertEqual(expected, solution.findDiagonalOrder(*params))
+
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ problems/n1424_diagonal_traverse_ii.py
@@ -0,0 +1,46 @@
+import unittest
+from unittest_data_provider import data_provider  # type: ignore
+
+
+class Solution:
+    """
+    https://leetcode.com/problems/diagonal-traverse-ii/
+    difficulty: medium
+    """
+
+    def findDiagonalOrder(self, nums: list[list[int]]) -> list[int]:
+        result: list[list[int]] = []
+
+        for m in range(len(nums)):
+            for n in range(len(nums[m])):
+                if m + n == len(result):
+                    result.append([])
+                result[m + n].append(nums[m][n])
+
+        return [num for row in result for num in reversed(row)]
+
+
+class SolutionTestCase(unittest.TestCase):
+    @staticmethod
+    def dataprovider():
+        yield (
+            # params
+            [[[1, 2, 3], [4, 5, 6], [7, 8, 9]]],
+            # expected
+            [1, 4, 2, 7, 5, 3, 8, 6, 9],
+        )
+        yield (
+            # params
+            [[[1, 2, 3, 4, 5], [6, 7], [8], [9, 10, 11], [12, 13, 14, 15, 16]]],
+            # expected
+            [1, 6, 2, 8, 7, 3, 9, 4, 12, 10, 5, 13, 11, 14, 15, 16],
+        )
+
+    @data_provider(dataprovider)
+    def test_solution(self, params, expected):
+        solution = Solution()
+        self.assertEqual(expected, solution.findDiagonalOrder(*params))
+
+
+if __name__ == '__main__':
+    unittest.main()
